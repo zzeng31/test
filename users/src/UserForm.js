6374423021bcedb402e8d1a4da93f089f2080c73
zzeng31 <zinuozeng@gmail.com>
2023-12-06 20:26:52 -0800

    commit B 1

--- users/src/UserForm.js
+++ users/src/UserForm.js
@@ -42,6 +42,31 @@ const UserForm = ({ onAddUser }) => {
         />
       </div>
       <div>
+        <label>LastName</label>
+        <input
+          value={lastName}
+          onChange={(e) => setLastName(e.target.value)}
+          type="text"
+        />
+      </div>
+      <div>
+        <label>Email</label>
+        <input
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          type="email"
+        />
+      </div>
+      <div>
+        <label>Phone</label>
+        <input
+          value={phone}
+          onChange={(e) => setPhone(e.target.value)}
+          type="number"
+        />
+      </div>
+      <button>Add User</button>
+      <div>
         <label>Phone</label>
         <input
           value={phone}
